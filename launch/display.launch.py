33d645511b072ab4e01e4e9e371a93689e3b22e6
DanielRobayo12 <daniel.felipe@gmail.com>
2025-09-30 18:36:07 -0500

    firstCommit

---
+++ launch/display.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('rosPackage')
+
+    # Ruta absoluta al URDF
+    urdf_file = os.path.join(pkg_share, 'urdf', 'peperURDF.urdf')
+    
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
+    # Ruta al archivo rviz guardado
+    rviz_config = os.path.join(pkg_share, 'rviz', 'ros.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_desc}]
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', rviz_config],   # 👈 ahora lee TU archivo
+            output='screen'
+        )
+    ])
